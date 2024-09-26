076576a23e7b047ce921bde5f951bf081862a721
Gregory Gerard <wako057@gmail.com>
2024-09-26 11:24:52 +0200

    feat: add descrive for test suites

--- src/components/Greeeting.test.js
+++ src/components/Greeeting.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test('renders Hello World as a test', () => {
-    render(<Greeting />);
-    const h2Element = screen.getByText('Hello World', { exact: false });
-    expect(h2Element).toBeInTheDocument();
+describe('<Greeting/> Component', () => {
+    test('renders Hello World as a test', () => {
+        render(<Greeting />);
+        const h2Element = screen.getByText('Hello World', { exact: false });
+        expect(h2Element).toBeInTheDocument();
+    })
 });
